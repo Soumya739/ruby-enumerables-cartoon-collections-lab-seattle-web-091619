4b07bca3dab891c3f4e3dc636bce8177e1a5c926
Soumya Goyal <somyagoyal739@gmail.com>
2019-09-08 19:43:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(name_array)
-  name_array.each_with_index(1) { |item, index| puts "#{index}. #{item}" }
+  name_array.each_with_index { |item, index| puts "#{index+1}. #{item}" }
 end
 
 def summon_captain_planet# code an argument here
